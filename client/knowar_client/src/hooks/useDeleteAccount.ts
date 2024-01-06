fc3a089923d4e2c1cf352c79b707a05b2469a9d7
Tests <michalisouroumis@gmail.com>
2024-01-06 18:31:29 +0100

    Update useDeleteAccount to ask for confirmation before deletes the account

--- client/knowar_client/src/hooks/useDeleteAccount.ts
+++ client/knowar_client/src/hooks/useDeleteAccount.ts
@@ -6,29 +6,44 @@ import {Alert} from 'react-native';
 export function useDeleteAccount() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
-  const {userId, logout} = useContext(AuthContext); // Assuming you have a logout function
+  const {userId, logout} = useContext(AuthContext);
 
-  const deleteAccount = useCallback(async () => {
-    setLoading(true);
-    setError(null);
+  const deleteAccount = useCallback(() => {
+    const confirmDelete = () => {
+      setLoading(true);
+      setError(null);
 
-    try {
-      const response = await deleteUser(userId);
+      deleteUser(userId)
+        .then(response => {
+          if (response.status === 'ok') {
+            Alert.alert(
+              'Success',
+              'Your account has been successfully deleted.',
+              [{text: 'OK', onPress: () => logout()}],
+            );
+          } else {
+            throw new Error('Failed to delete the account.');
+          }
+        })
+        .catch(err => {
+          if (err instanceof Error) {
+            setError(err);
+          }
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
 
-      if (response.status === 'ok') {
-        Alert.alert('Success', 'Your account has been successfully deleted.', [
-          {text: 'OK', onPress: () => logout()},
-        ]);
-      } else {
-        throw new Error('Failed to delete the account.');
-      }
-    } catch (err) {
-      if (err instanceof Error) {
-        setError(err);
-      }
-    } finally {
-      setLoading(false);
-    }
+    Alert.alert(
+      'Confirm Delete',
+      'Are you sure you want to delete your account?',
+      [
+        {text: 'Cancel', style: 'cancel'},
+        {text: 'Yes', onPress: confirmDelete},
+      ],
+      {cancelable: false},
+    );
   }, [userId, logout]);
 
   return {
