0bff1769284b0f10a6ff60fddf840a1b3e456614
Tests <michalisouroumis@gmail.com>
2024-05-04 20:21:09 +0200

    Create navigaiton screens enums

---
+++ client/knowar_client/src/types/navigation.ts
@@ -0,0 +1,17 @@
+export enum InitialScreens {
+  SplashScreen = 'SplashScreen',
+}
+
+export enum AuthenticatedScreens {
+  MainMenuScreen = 'MainMenuScreen',
+  MultiplayerLobbyScreen = 'MultiplayerLobbyScreen',
+  GameScreen = 'GameScreen',
+  CreateGameScreen = 'CreateGameScreen',
+  SinglePlayerCreateGameScreen = 'SinglePlayerCreateGameScreen',
+  AccountScreen = 'AccountScreen',
+}
+
+export enum UnauthenticatedScreens {
+  LoginScreen = 'LoginScreen',
+  RegisterScreen = 'RegisterScreen',
+}
