d1591dad3d40de4ee690e303907557789f9e0321
Tests <michalisouroumis@gmail.com>
2024-05-04 18:24:44 +0200

    Move useRetrieveCredentials to App.tsx

--- client/knowar_client/src/App.tsx
+++ client/knowar_client/src/App.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import AuthContextProvider from './store/auth-context';
 import {Navigation} from './navigation/Navigation';
 import GameProvider from './store/GameContext';
+import {useRetrieveCredentials} from './hooks/useRetrieveCredentials';
+import LoadingScreen from './screens/LoadingScreen';
 
 export default function App() {
   return (
     <AuthContextProvider>
       <GameProvider>
-        <Navigation />
+        <AppContent />
       </GameProvider>
     </AuthContextProvider>
   );
+}
+
+function AppContent() {
+  const isLoading = useRetrieveCredentials();
+
+  if (isLoading) {
+    return <LoadingScreen text="Loading..." />;
+  }
+
+  return <Navigation />;
 }
--- client/knowar_client/src/navigation/Navigation.tsx
+++ client/knowar_client/src/navigation/Navigation.tsx
@@ -1,23 +1,16 @@
 import React, {useContext} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import LoadingScreen from '../screens/LoadingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import {DrawerNavigator} from './DrawerNavigator';
 import SplashScreen from '../screens/SplashScreen';
-import {useRetrieveCredentials} from '../hooks/useRetrieveCredentials';
 import {AuthContext} from '../store/auth-context';
 
 const Stack = createNativeStackNavigator();
 
 export function Navigation() {
   const authCtx = useContext(AuthContext);
-  const isLoading = useRetrieveCredentials();
-
-  if (isLoading) {
-    return <LoadingScreen text="Loading..." />;
-  }
 
   return (
     <NavigationContainer>
