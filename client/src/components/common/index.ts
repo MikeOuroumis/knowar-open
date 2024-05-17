ee897051d9999cdf897de9d613c646a9e39a00a8
Tests <michalisouroumis@gmail.com>
2024-05-17 11:02:29 +0200

    Delete DropdownComponent because it is not used

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,4 +1,3 @@
 export {default as ButtonComponent} from './ButtonComponent';
-export {default as DropdownComponent} from './DropdownComponent';
 export {default as Input} from './Input';
 export {default as GoBackArrow} from './GoBackArrow';
